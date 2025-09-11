f205352fff2eb372af618f7fc9abc19d76ca2275
mjLee6207 <dlalswnd34@gmail.com>
2025-09-11 16:29:12 +0900

    2차 리펙토링(jsp해결전)
    11

--- CheForest/src/main/java/com/simplecoding/cheforest/file/entity/File.java
+++ CheForest/src/main/java/com/simplecoding/cheforest/file/entity/File.java
@@ -23,22 +23,16 @@ public class File extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "FILE_SEQ_JPA")
     private Long id;  // 파일 ID (PK)
 
-    @Column(nullable = false, length = 200)
     private String fileName;  // 원본 파일명
 
-    @Column(nullable = false, length = 300)
     private String filePath;  // 저장 경로
 
-    @Column(nullable = false, length = 20)
     private String fileType;  // 파일 확장자
 
-    @Column(nullable = false, length = 20)
     private String useType;   // 사용 목적 (예: BOARD, PROFILE)
 
-    @Column(nullable = false)
     private Long useTargetId; // 대상 ID (게시글 번호 등)
 
-    @Column(length = 50)
     private String usePosition; // 위치 구분 (썸네일, 본문 등)
 
 
